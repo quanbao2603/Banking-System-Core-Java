b9ca27c8037f5f729c4a4b92c504a0fb0173678e
quanbao2603 <nguyenquanbao2632005@gmail.com>
2024-10-18 03:48:33 +0700

    update

--- Account.java
+++ Account.java
@@ -1,3 +1,29 @@
-public class Account {
 
+import java.util.ArrayList;
+import java.util.List;
+    
+public class Account {
+    private String accountNumber;
+    private String customer;
+    private double  balance;
+    private List<Transaction> transactions;
+    public Account(String accountNumber, String customer, double balance){
+        this.accountNumber = accountNumber;
+        this.customer = customer;
+        this.balance = balance;
+        this.transactions = new ArrayList<>();
+    }
+
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+    public String getCustomer(){
+        return customer;
+    }
+    public Double getBalance(){
+        return balance;
+    }
+    public List<Transaction> getTransactions(){
+        return transactions;        
+    }
 }
